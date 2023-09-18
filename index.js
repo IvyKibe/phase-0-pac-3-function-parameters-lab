8c42ab273540fb7781df9a159a1b61595b75487d
ivykibe <maleeivy@gmail.com>
2023-09-18 14:55:19 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+function sayHelloTo(Ivy = "IvyKibe") {
+    console.log(`Hello, ${Ivy}!`);
+  }
+
+  function introductionWithLanguageOptional(name = "language") {
+    console.log(`Hello, ${JavaScript}!`);
+  }
